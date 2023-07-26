ef4871f9411627df4e34f7e644f1728f15831aab
kachers <renarskacerovskis@gmail.com>
2023-07-26 13:26:35 +0200

    Exercise 1 & 7 completed

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi tekstu");
+            string input = Console.ReadLine();
+            int result = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsUpper(input[i]))
+                {
+                    result++;
+                }            
+            }
+            Console.WriteLine($"{result}");
+            Console.ReadKey();
+        }
+
+    }
+}
