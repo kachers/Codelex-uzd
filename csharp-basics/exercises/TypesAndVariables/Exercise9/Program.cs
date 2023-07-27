e8fa0cba1e010a0e562c59810c92cf96232db0ae
kachers <renarskacerovskis@gmail.com>
2023-07-27 15:58:41 +0200

    Exercise9 correction

--- csharp-basics/exercises/TypesAndVariables/Exercise9/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise9/Program.cs
@@ -14,17 +14,13 @@ namespace Exercise9
             Console.WriteLine("Please provide minutes");
             int minutes = int.Parse(Console.ReadLine());
             Console.WriteLine("Please provide seconds");
-            int seconds = int.Parse(Console.ReadLine());
-            Console.WriteLine($"{hours}/3600 + {minutes}60 + {seconds}");          
+            int seconds = int.Parse(Console.ReadLine());                    
             int totalSeconds = hours*3600 + minutes*60 + seconds;
-            decimal speedMs = (decimal)distance / totalSeconds;
-            decimal speedKmh = speedMs * 3600 / 1000;
-            decimal speedMph = speedKmh * (decimal)0.621371192;
+            float speedMs = (float)distance / totalSeconds;
+            float speedKmh = speedMs * 3600 / 1000;
+            float speedMph = speedKmh * (float)0.621371192;
 
-
-
-
-            Console.WriteLine($"Your speed in meters/second is{speedMs}");
+            Console.WriteLine($"Your speed in meters/second is {speedMs}");
             Console.WriteLine($"Your speed in km/h is {speedKmh}");
             Console.WriteLine($"Your speed in miles/h is {speedMph}");
             Console.ReadKey();
