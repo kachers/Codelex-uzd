4e8013e4f438de90d2c86ec4c8ebc5e897029aa4
kachers <renarskacerovskis@gmail.com>
2023-08-03 12:39:59 +0200

    Exercise6 completed

---
+++ csharp-basics/exercises/Arrays/Exercise6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise6
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] tenInt = new int[10];
+            int[] tenIntCopy = new int[10];
+            for (int i=0; i<tenInt.Length; i++) { 
+                Random random = new();
+                tenInt[i] = random.Next(1,100);
+            }
+
+            for (int i = 0; i < tenInt.Length; i++)
+            {
+                tenIntCopy[i] = tenInt[i];
+            }
+
+            tenInt[tenInt.Length-1] = -7;
+            Console.WriteLine(string.Join(" ", tenInt));
+            Console.WriteLine(string.Join(" ", tenIntCopy));
+        }
+    }
+}
