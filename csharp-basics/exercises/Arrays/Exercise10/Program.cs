4fc8c00a6712b35831b64d0fc2e486b41e91024b
kachers <renarskacerovskis@gmail.com>
2023-08-03 15:24:12 +0200

    Exercise10 completed

---
+++ csharp-basics/exercises/Arrays/Exercise10/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise10;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(CountPosSumNeg(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15 }));
+    }
+
+    private static int[] CountPosSumNeg(int[] pos)
+    {
+        var posCount = 0;
+        var negSum = 0;
+        foreach (var i in pos)
+            if (i > 0) posCount++;
+            else negSum += i;
+
+        if (posCount > 0 || negSum > 0)
+        {
+            int[] result = { posCount, negSum };
+            return result;
+        }
+
+        var emptyArray = new int[0];
+        return emptyArray;
+    }
+}
