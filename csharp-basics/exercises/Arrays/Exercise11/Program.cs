04b3bc700225567ccab3b137d8c9352a3cc799c2
kachers <renarskacerovskis@gmail.com>
2023-08-03 15:39:29 +0200

    Exercise11 completed

---
+++ csharp-basics/exercises/Arrays/Exercise11/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise11;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(FindNemo("Nemo is me"));
+    }
+
+    private static string FindNemo(string text)
+    {
+        var textArr = text.Split(" ");
+        for (var i = 0; i < textArr.Length; i++)
+            if (textArr[i].Equals("Nemo"))
+                return $"I found Nemo at {i + 1}!";
+
+        return "I can't find Nemo :(";
+    }
+}
