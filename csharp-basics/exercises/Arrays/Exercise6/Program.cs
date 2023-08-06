4ed21dce18369b989e7a4fca2e2c2667d8e05621
kachers <renarskacerovskis@gmail.com>
2023-08-06 20:42:06 +0200

    Exercise6 correction

--- csharp-basics/exercises/Arrays/Exercise6/Program.cs
+++ csharp-basics/exercises/Arrays/Exercise6/Program.cs
@@ -1,24 +1,24 @@
-﻿namespace Exercise6
+﻿namespace Exercise6;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
+        var tenInt = new int[10];
+        var tenIntCopy = new int[10];
+        for (var i = 0; i < tenInt.Length; i++)
         {
-            int[] tenInt = new int[10];
-            int[] tenIntCopy = new int[10];
-            for (int i=0; i<tenInt.Length; i++) { 
-                Random random = new();
-                tenInt[i] = random.Next(1,100);
-            }
-
-            for (int i = 0; i < tenInt.Length; i++)
-            {
-                tenIntCopy[i] = tenInt[i];
-            }
-
-            tenInt[tenInt.Length-1] = -7;
-            Console.WriteLine(string.Join(" ", tenInt));
-            Console.WriteLine(string.Join(" ", tenIntCopy));
+            Random random = new();
+            tenInt[i] = random.Next(1, 100);
         }
+
+        for (var i = 0; i < tenInt.Length; i++)
+        {
+            tenIntCopy[i] = tenInt[i];
+        }
+
+        tenInt[tenInt.Length - 1] = -7;
+        Console.WriteLine($"Array 1:{string.Join(" ", tenInt)}");
+        Console.WriteLine($"Array 2:{string.Join(" ", tenIntCopy)}");
     }
 }
