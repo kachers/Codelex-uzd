74f13579cfd8524f907114b832935a25e26cb882
kachers <renarskacerovskis@gmail.com>
2023-08-08 17:51:53 +0200

    Exercise5 completed

---
+++ csharp-basics/exercises/Loops/Exercise5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise5;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var lineLength = 30;
+        Console.WriteLine("Enter first word: ");
+        var firstWord = Console.ReadLine();
+        Console.WriteLine("Enter second word: ");
+        var secondWord = Console.ReadLine();
+
+        var numberOfDots = lineLength - (firstWord.Length + secondWord.Length);
+
+        Console.Write(firstWord);
+        for (var i = 0; i < numberOfDots; i++) Console.Write(".");
+        Console.Write(secondWord);
+    }
+}
