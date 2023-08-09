0221bcc8b4bd5eceb3426d5d1978c488782d9df9
kachers <renarskacerovskis@gmail.com>
2023-08-09 16:05:17 +0200

    Exercise11 completed

---
+++ csharp-basics/exercises/Loops/Exercise11/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise11;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(ReverseCase("Happy Birthday"));
+    }
+
+    private static string ReverseCase(string str)
+    {
+        var result = string.Empty;
+        var inputArray = str.ToCharArray();
+
+        foreach (var c in inputArray)
+        {
+            if (char.IsLower(c)) result += c.ToString().ToUpper();
+            else if (char.IsUpper(c)) result += c.ToString().ToLower();
+            else result += c.ToString();
+        }
+
+        return result;
+    }
+}
