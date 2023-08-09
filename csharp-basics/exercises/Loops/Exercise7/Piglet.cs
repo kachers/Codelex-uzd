1154f8c1f31ae37412c5d285398e4f7ea87ef176
kachers <renarskacerovskis@gmail.com>
2023-08-09 10:49:58 +0200

    Exercise7 completed

--- csharp-basics/exercises/Loops/Exercise7/Piglet.cs
+++ csharp-basics/exercises/Loops/Exercise7/Piglet.cs
@@ -1,10 +1,39 @@
-﻿namespace Exercise7
+﻿namespace Exercise7;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Welcome to Piglet!");
+        var random = new Random();
+        var points = 0;
+        char userChoice;
+
+        do
         {
-            Console.WriteLine("Hello, World!");
+            var roll = random.Next(1, 7);
+            if (roll == 1)
+            {
+                points = 0;
+                Console.WriteLine("\nYou rolled a 1!");
+                break;
+            }
+
+            Console.WriteLine($"\nYou rolled a {roll}!");
+            points += roll;
+
+            while (true)
+            {
+                Console.WriteLine("\nRoll again? y/n ");
+                userChoice = char.ToLower(Console.ReadKey().KeyChar);
+
+                if (userChoice == 'y' || userChoice == 'n') break;
+                Console.WriteLine("\nInvalid choice. Please enter y or n.");
+            }
+        } while (userChoice == 'y');
+        {
+            Console.WriteLine($"\nYou got {points} points.");
+            Console.WriteLine("Game Over");
         }
     }
 }
