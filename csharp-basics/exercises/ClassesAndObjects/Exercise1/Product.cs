032305f6c99368ccd380ba9b1f6b8f5df38f44a8
kachers <renarskacerovskis@gmail.com>
2023-08-10 19:54:41 +0200

    Exercise1 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise1;
+
+internal class Product
+{
+    private int amount;
+    private string name;
+    private double price;
+
+    public Product(string name, double priceAtStart, int amountAtStart)
+    {
+        this.name = name;
+        this.price = priceAtStart;
+        this.amount = amountAtStart;
+    }
+
+    public void PrintProduct()
+    {
+        Console.WriteLine($"{name}, price {price} EUR, amount {amount} units");
+    }
+
+    public void ChangeAmount(int newAmount)
+    {
+        amount = newAmount;
+    }
+
+    public void ChangePrice(double newPrice)
+    {
+        price = newPrice;
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product("Logitech mouse", 70.00, 14);
+            Product phone = new Product("Iphone 5s", 999.99, 3);
+            Product printer = new Product("Epson EB-U05", 440.46, 1);
+
+            mouse.PrintProduct();
+            phone.PrintProduct();
+            printer.PrintProduct();
+
+            printer.ChangePrice(420.20);
+            printer.ChangeAmount(2);
+            Console.WriteLine("\nProduct - printer after changes");
+            printer.PrintProduct();
+        }
+    }
+}
