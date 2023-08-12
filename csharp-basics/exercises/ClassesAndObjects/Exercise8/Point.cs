0cab277ef554c24ff8c06e9519677bd23a3aaeb2
kachers <renarskacerovskis@gmail.com>
2023-08-12 22:05:20 +0200

    Exercise8 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Point.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise8;
+
+internal class Point
+{
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public int Getx()
+    {
+        return x;
+    }
+
+    public int Gety()
+    {
+        return y;
+    }
+
+    public void Setx(int x)
+    {
+        this.x = x;
+    }
+
+    public void Sety(int y)
+    {
+        this.y = y;
+    }
+}
