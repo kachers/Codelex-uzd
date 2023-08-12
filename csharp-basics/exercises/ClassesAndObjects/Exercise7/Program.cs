8d182637dba3a77cf5b8488a1abd2a140b159100
kachers <renarskacerovskis@gmail.com>
2023-08-12 21:37:37 +0200

    Exercise7 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Exercise7;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var startingBalance = ReadDoubleInput("How much money is in the account?:");
+        var annualInterestRate = ReadDoubleInput("Enter the annual interest rate:");
+        var monthsPassed = (int)ReadDoubleInput("How long has the account been opened?");
+        var acc1 = new SavingsAccount(startingBalance);
+        acc1.annualInterestRate = annualInterestRate;
+
+        double totalDeposits = 0;
+        double totalWithdrawals = 0;
+        double totalInterestAdded = 0;
+
+        for (var i = 1; i <= monthsPassed; i++)
+        {
+            var depositedAmount = ReadDoubleInput($"Enter amount deposited for month: {i}:");
+            acc1.Deposit(depositedAmount);
+            totalDeposits += depositedAmount;
+
+            var withdrawAmount = ReadDoubleInput($"Enter amount withdrawn for month: {i}:");
+            acc1.Withdraw(withdrawAmount);
+            totalWithdrawals -= withdrawAmount;
+
+            totalInterestAdded += acc1.GetMonthlyInterestAmount();
+            acc1.AddMonthlyInterest();
+
+            if (i == monthsPassed)
+                Console.WriteLine($"\nTotal deposited: {totalDeposits:C2}\n" +
+                                  $"Total withdrawn: {Math.Abs(totalWithdrawals):C2}\n" +
+                                  $"Interest earned: {totalInterestAdded:C2}\n" +
+                                  $"Ending balance: {acc1.GetBalance():C2}");
+        }
+    }
+
+    private static double ReadDoubleInput(string prompt)
+    {
+        double value;
+        while (true)
+        {
+            Console.Write(prompt);
+            if (double.TryParse(Console.ReadLine(), out value)) return value;
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/SavingsAccount.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercise7;
+
+internal class SavingsAccount
+{
+    private double balance;
+    public double annualInterestRate;
+
+    public SavingsAccount(double startBal)
+    {
+        this.balance = startBal;
+    }
+
+    public void Withdraw(double amount)
+    {
+        balance -= amount;
+    }
+
+    public void Deposit(double amount)
+    {
+        balance += amount;
+    }
+
+    public void AddMonthlyInterest()
+    {
+        var monthlyInterestRate = balance * annualInterestRate / 12;
+        balance += monthlyInterestRate;
+    }
+
+    public double GetBalance()
+    {
+        return balance;
+    }
+
+    public double GetMonthlyInterestAmount()
+    {
+        var monthlyInterestRate = balance * annualInterestRate / 12;
+        return monthlyInterestRate;
+    }
+}
