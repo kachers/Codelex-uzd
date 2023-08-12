29739054e1d3e12f979df2489dbe78f20411a7a3
kachers <renarskacerovskis@gmail.com>
2023-08-12 17:54:58 +0200

    Exercise6 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Dog.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise6;
+
+internal class Dog
+{
+    public Dog father;
+    public Dog mother;
+    public string name;
+    public string sex;
+
+    public Dog(string name, string sex)
+    {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public string FathersName()
+    {
+        return father == null ? "Unknown" : father.name;
+    }
+
+    public bool HasSameMotherAs(Dog otherDog)
+    {
+        return mother == otherDog.mother ? true : false;
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercise6;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var dog1 = new Dog("Max", "male");
+        var dog2 = new Dog("Rocky", "male");
+        var dog3 = new Dog("Sparky", "male");
+        var dog4 = new Dog("Buster", "male");
+        var dog5 = new Dog("Sam", "male");
+        var dog6 = new Dog("Lady", "female");
+        var dog7 = new Dog("Molly", "female");
+        var dog8 = new Dog("Coco", "female");
+
+        dog1.mother = dog6;
+        dog1.father = dog2;
+        dog2.mother = dog7;
+        dog2.father = dog5;
+        dog4.mother = dog6;
+        dog4.father = dog3;
+        dog8.mother = dog7;
+        dog8.father = dog4;
+
+        Console.WriteLine($"Should return Buster: \n actually returns : {dog8.FathersName()}");
+        Console.WriteLine($"\nShould return Unknown: \n actually returns : {dog3.FathersName()}");
+        Console.WriteLine($"\nIf Coco has the same mother as Rocky" +
+                          $" it should return True: \n actually returns : {dog8.HasSameMotherAs(dog2)}");
+    }
+}
