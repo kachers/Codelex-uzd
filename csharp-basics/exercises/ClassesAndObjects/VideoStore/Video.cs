7fc5cd5c3ba2b5f39f6625d8b808b922beb9299a
kachers <renarskacerovskis@gmail.com>
2023-08-13 15:57:09 +0200

    Exercise10 completed

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/Video.cs
@@ -1,7 +1,43 @@
-﻿namespace VideoStore
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VideoStore
 {
-    class Video
+    internal class Video
     {
 
+        private readonly List<double> _ratings;
+
+        public Video(string title)
+        {
+            Title = title;
+            IsAvailable = true;
+            _ratings = new List<double>();
+        }
+
+        public string Title { get; }
+        public bool IsAvailable { get; private set; }
+        public double AverageRating => _ratings.Any() ? _ratings.Average() : 0;
+
+        public void BeingCheckedOut()
+        {
+            if (IsAvailable) IsAvailable = false;
+        }
+
+        public void BeingReturned()
+        {
+            if (!IsAvailable) IsAvailable = true;
+        }
+
+        public void ReceiveRating(double rating)
+        {
+            _ratings.Add(rating);
+        }
+
+        public override string ToString()
+        {
+            return $"Title: {Title} | Average Rating: {AverageRating} | Available: {IsAvailable}";
+        }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
@@ -1,8 +1,59 @@
-﻿namespace VideoStore
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VideoStore
 {
-    class VideoStore
+    internal class VideoStore
     {
+        private readonly List<Video> _inventory;
+        
 
+        public VideoStore()
+        {
+            _inventory = new List <Video>();
+        }
 
+        public void AddToInventory(string title)
+        {
+            _inventory.Add(new Video(title));
+        }
+
+        public void CheckOut(string title)
+        {
+            foreach (var t in _inventory.Where(t => t.Title == title))
+            {
+                t.BeingCheckedOut();
+                break;
+            }
+        }
+
+        public void ReturnVideo(string title)
+        {
+            foreach (var t in _inventory.Where(t => t.Title == title))
+            {
+                t.BeingReturned();
+                break;
+            }
+        }
+
+        public void ReceiveRating(string title, double rating)
+        {
+            foreach (var t in _inventory.Where(t => t.Title == title))
+            {
+                t.ReceiveRating(rating);
+                break;
+            }
+        }
+
+        public void ListAvailableVideos()
+        {
+            Console.WriteLine();
+            foreach (var t in _inventory)
+            {
+                Console.WriteLine(t);
+            }
+            Console.WriteLine();
+        }
     }
 }
