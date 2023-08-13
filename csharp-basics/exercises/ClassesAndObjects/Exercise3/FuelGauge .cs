d89ffbb360ce380f8943402c0c5996b1601c43b8
kachers <renarskacerovskis@gmail.com>
2023-08-13 23:50:27 +0200

    Exercise3 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/FuelGauge .cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise3;
+
+public class FuelGauge
+{
+    public int FuelAmount;
+
+    public FuelGauge()
+    {
+        FuelAmount = 0;
+    }
+
+    public int GetFuelAmount()
+    {
+        return FuelAmount;
+    }
+
+    public void FillUp(int amount)
+    {
+        FuelAmount += amount;
+    }
+
+    public int BurnFuel()
+    {
+        return FuelAmount--;
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Odometer .cs
@@ -0,0 +1,37 @@
+﻿namespace Exercise3;
+
+public class Odometer
+{
+    public int counter;
+    public int CurrentMileage;
+    public FuelGauge fuelgauge;
+
+    public Odometer(FuelGauge fuelGauge, int mileage)
+    {
+        fuelgauge = fuelGauge;
+        CurrentMileage = mileage;
+        counter = 0;
+    }
+
+    public int GetCurrentMileage()
+    {
+        return CurrentMileage;
+    }
+
+    public void IncrementMileage()
+    {
+        if (CurrentMileage == 999999) CurrentMileage = 0;
+        CurrentMileage++;
+        counter++;
+        DecrementFuel();
+    }
+
+    public void DecrementFuel()
+    {
+        if (counter == 10)
+        {
+            fuelgauge.BurnFuel();
+            counter = 0;
+        }
+    }
+}
