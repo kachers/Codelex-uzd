492d1440bbc75073c5dd864866380fca885ce262
kachers <renarskacerovskis@gmail.com>
2023-08-13 18:47:18 +0200

    Exercise13 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise13/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise13;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var s1 = new Smoothie(new List<string> { "Banana" });
+        Console.WriteLine($"Ingredients: {string.Join(", ", s1.Ingredients)}");
+        Console.WriteLine($"Cost: {s1.GetCost():C2}");
+        Console.WriteLine($"Price: {s1.GetPrice():C2}");
+        Console.WriteLine($"Name: {s1.GetName()}");
+
+        var s2 = new Smoothie(new List<string> { "Raspberries", "Strawberries", "Blueberries" });
+        Console.WriteLine($"Ingredients: {string.Join(", ", s2.Ingredients)}");
+        Console.WriteLine($"Cost: {s2.GetCost():C2}");
+        Console.WriteLine($"Price: {s2.GetPrice():C2}");
+        Console.WriteLine($"Name: {s2.GetName()}");
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise13/Smoothie.cs
@@ -0,0 +1,59 @@
+﻿namespace Exercise13;
+
+internal class Smoothie
+{
+    public readonly List<string> Ingredients;
+
+    public Smoothie(List<string> ingredients)
+    {
+        Ingredients = ingredients;
+    }
+
+    public double GetCost()
+    {
+        double totalCost = 0;
+        foreach (var fruit in Ingredients)
+            switch (fruit)
+            {
+                case "Strawberries":
+                    totalCost += 1.5;
+                    break;
+                case "Banana":
+                    totalCost += 0.5;
+                    break;
+                case "Mango":
+                    totalCost += 2.5;
+                    break;
+                case "Blueberries":
+                    totalCost += 1;
+                    break;
+                case "Raspberries":
+                    totalCost += 1;
+                    break;
+                case "Apple":
+                    totalCost += 1.75;
+                    break;
+                case "Pineapple":
+                    totalCost += 3.5;
+                    break;
+            }
+
+        return totalCost;
+    }
+
+    public double GetPrice()
+    {
+        var cost = GetCost();
+        var price = cost + cost * 1.5;
+        return Math.Round(price, 2);
+    }
+
+    public string GetName()
+    {
+        var name = string.Empty;
+        Ingredients.Sort();
+        foreach (var fruit in Ingredients) name += fruit.Replace("berries", "berry") + " ";
+
+        return Ingredients.Count < 2 ? $"{name + "Smoothie"}" : $"{name + "Fusion"}";
+    }
+}
