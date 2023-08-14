4da08d1a3529382fc91c03f3541ba42ea66ac622
kachers <renarskacerovskis@gmail.com>
2023-08-15 00:39:41 +0200

    DecryptNumber completed

--- csharp-basics/exercises/Collections/DecryptNumber/Program.cs
+++ csharp-basics/exercises/Collections/DecryptNumber/Program.cs
@@ -5,8 +5,7 @@ namespace DecryptNumber
 {
     internal class Program
     {
-        // look at the keyboard.
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var cryptedNumbers = new List<string>
             {
@@ -17,6 +16,58 @@ namespace DecryptNumber
                 "!)(^&(#@",
                 "!)(#&%(*@#%"
             };
+
+            Console.WriteLine(DecryptNumber(cryptedNumbers));
+        }
+
+        public static List<string> DecryptNumber(List<string> cryptedNumbers)
+        {
+            List<string> numbersList = new();
+            for (var i = 0; i < cryptedNumbers.Count; i++)
+            {
+                var line = string.Empty;
+                foreach (var c in cryptedNumbers[i])
+                {
+                    var s = c.ToString();
+                    switch (s)
+                    {
+                        case "!":
+                            line += "1";
+                            break;
+                        case "@":
+                            line += "2";
+                            break;
+                        case "#":
+                            line += "3";
+                            break;
+                        case "$":
+                            line += "4";
+                            break;
+                        case "%":
+                            line += "5";
+                            break;
+                        case "^":
+                            line += "6";
+                            break;
+                        case "&":
+                            line += "7";
+                            break;
+                        case "*":
+                            line += "8";
+                            break;
+                        case "(":
+                            line += "9";
+                            break;
+                        case ")":
+                            line += "0";
+                            break;
+                    }
+                }
+
+                numbersList.Add(line);
+            }
+
+            return numbersList;
         }
     }
 }
