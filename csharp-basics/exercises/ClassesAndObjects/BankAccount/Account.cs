b9ca1a6d5ca8bbcec6b76a236083257ff818cb77
kachers <renarskacerovskis@gmail.com>
2023-08-14 20:51:32 +0200

    Exercise9 completed

---
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class Account
+    {
+        private string _name;
+        private double _balance;
+
+        public Account(string name, double balance)
+        {
+            _name = name;
+            _balance = balance;
+        }
+
+        public string Name { get{return _name; } }
+        public double Balance { get { return _balance; } }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        } 
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/BankAccount/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace BankAccount
+﻿using System;
+
+namespace BankAccount
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Account Tim = new Account("Tim", 300.145);
+            Console.WriteLine(ShowUserNameAndBalance(Tim));
+        }
+
+        static string ShowUserNameAndBalance(Account Benben)
+        {
+            var name = Benben.GetName();
+            var balance = Benben.GetBalance();
+            return balance < 0 ? $"{name}, -{Math.Abs(balance).ToString("C2")}" : $"{name}, {balance.ToString("C2")}";
         }
     }
 }
