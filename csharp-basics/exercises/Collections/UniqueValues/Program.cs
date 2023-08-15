4119bd15ec49dfa2eb9cf7ca52ee7deab818e8fd
kachers <renarskacerovskis@gmail.com>
2023-08-15 09:59:25 +0200

    UniqueValues completed

--- csharp-basics/exercises/Collections/UniqueValues/Program.cs
+++ csharp-basics/exercises/Collections/UniqueValues/Program.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UniqueValues
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //ToDo: Given a non-empty list of strings, return a list that contains only unique (non-duplicate) strings.
-            //ToDo: Example: ["abc", "xyz", "klm", "xyz", "abc", "abc", "rst"] → ["klm", "rst"]
-
             var values = new List<string> { "Hi", "Meow", "Hello", "Meow", "Hi!", "Meow", "Hi", "Bye" };
-
+            var result = (from value in values
+                let count = values.Count(str => str == value)
+                where count == 1
+                select value).ToList();
+            Console.WriteLine(string.Join(",", result));
         }
     }
 }
