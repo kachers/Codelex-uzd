c0fd2625d316fe21d7b92e17874667d95070cd2d
kachers <renarskacerovskis@gmail.com>
2023-08-15 09:39:16 +0200

    ReplaceSubstring completed

--- csharp-basics/exercises/Collections/ReplaceSubstring/Program.cs
+++ csharp-basics/exercises/Collections/ReplaceSubstring/Program.cs
@@ -1,12 +1,14 @@
-﻿namespace ReplaceSubstring
+﻿using System.Linq;
+
+namespace ReplaceSubstring
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var words = new[] { "near", "speak", "tonight", "weapon", "customer", "deal", "lawyer" };
-            //ToDo: Write a query that replaces 'ea' substring with astersik (*) in given list of words.
-            //ToDo: "learn", "current", "deal" →  "l*rn", "current", "d*l"
+
+            var transformedWords = words.Select(word => word.Replace("ea", "*")).ToArray();
         }
     }
 }
