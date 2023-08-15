5f467b180adebc87c0f3b69f154f4eccc0f4c852
kachers <renarskacerovskis@gmail.com>
2023-08-15 16:37:43 +0200

    Exercise4 completed

---
+++ csharp-basics/exercises/Collections/Exercise4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise4;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> names = new();
+        var userInput = string.Empty;
+
+        while (true)
+        {
+            Console.WriteLine("Enter name:");
+            userInput = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(userInput)) break;
+
+            names.Add(userInput);
+        }
+
+        List<string> filteredNames = new();
+        foreach (var name in names.Where(name => !filteredNames.Contains(name))) filteredNames.Add(name);
+
+        Console.WriteLine("Unique name list contains:" + string.Join(" ", filteredNames));
+    }
+}
