44f25b9bf252f571ea9077a5fd1957a63cad61c3
kachers <renarskacerovskis@gmail.com>
2023-08-16 11:23:32 +0200

    Exercise6 completed

--- csharp-basics/exercises/Collections/WordCount/Program.cs
+++ csharp-basics/exercises/Collections/WordCount/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
-namespace WordCount
+namespace WordCount;
+
+public class Program
 {
-    class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var filePath = "C:/Users/john/source/repos/Codelex-uzd/csharp-basics/exercises/Collections/WordCount/lear.txt";
+
+        if (File.Exists(filePath))
         {
-            Console.WriteLine("Hello World!");
+            var lines = File.ReadAllLines(filePath);
+            var lineCount = lines.Length;
+            var wordCount = lines
+                .SelectMany(line => line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries))
+                .Count(word => word.All(char.IsLetterOrDigit));
+            var charCount = lines.Sum(line => line.Length);
+
+            Console.WriteLine($"Lines = {lineCount}");
+            Console.WriteLine($"Words = {wordCount}");
+            Console.WriteLine($"Chars = {charCount}");
+        }
+        else
+        {
+            Console.WriteLine("File not found.");
         }
     }
 }
