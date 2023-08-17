00c742cc9a8e6ae7db2a68829e4f34f54d6473b1
kachers <renarskacerovskis@gmail.com>
2023-08-17 23:30:40 +0200

    Exercise5 completed

---
+++ csharp-basics/exercises/Collections/Exercise5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Exercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number.");
+            var number = Console.ReadLine();
+            var tempNumber = number;
+
+            do
+            {
+                var numbers = tempNumber.ToCharArray();
+                double sum = 0;
+                foreach (var num in numbers)
+                {
+                    sum += Math.Pow(char.GetNumericValue(num), 2);
+                }
+
+                tempNumber = sum.ToString();
+            } while (tempNumber != "4" && tempNumber != "1");
+
+            Console.WriteLine(tempNumber == "1" ? "happy" : "not happy");
+            Console.ReadKey();
+        }
+    }
+}
