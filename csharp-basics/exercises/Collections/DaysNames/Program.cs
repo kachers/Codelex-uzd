ec85c04fb57ffa8ccee3173c49abf49e40b9475b
kachers <renarskacerovskis@gmail.com>
2023-08-18 19:03:19 +0200

    DaysNames completed

--- csharp-basics/exercises/Collections/DaysNames/Program.cs
+++ csharp-basics/exercises/Collections/DaysNames/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-//ToDo: Write a query that returns names of days
-//https://docs.microsoft.com/en-us/dotnet/api/system.dayofweek?view=net-6.0
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace DaysNames
+{ class Program
+    { private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year ");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter month ");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter day");
+            int day = int.Parse(Console.ReadLine());
+
+            DateTime dt = new DateTime(year, month, day);
+            var LV = new System.Globalization.CultureInfo("lv-LV");
+            string diena = LV.DateTimeFormat.GetDayName(dt.DayOfWeek);
+            Console.WriteLine($"The day of the {day}/{month}/{year} was: {diena}");
+        }
+    }
+}
+
+
+
