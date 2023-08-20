f04b5b07421ff2f2748d4bc2b6e243bccd481bbe
kachers <renarskacerovskis@gmail.com>
2023-08-20 18:19:05 +0200

    Exercise8 correction

--- csharp-basics/exercises/ClassesAndObjects/Exercise8/Point.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Point.cs
@@ -4,30 +4,10 @@ internal class Point
 {
     public Point(int x, int y)
     {
-        this.x = x;
-        this.y = y;
+        X = x;
+        Y = y;
     }
 
-    public int x { get; set; }
-    public int y { get; set; }
-
-    public int Getx()
-    {
-        return x;
-    }
-
-    public int Gety()
-    {
-        return y;
-    }
-
-    public void Setx(int x)
-    {
-        this.x = x;
-    }
-
-    public void Sety(int y)
-    {
-        this.y = y;
-    }
+    public int X { get; set; }
+    public int Y { get; set; }
 }
--- csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
@@ -7,19 +7,19 @@ internal class Program
         var p1 = new Point(5, 2);
         var p2 = new Point(-3, 6);
         SwapPoints(p1, p2);
-        Console.WriteLine("(" + p1.x + ", " + p1.y + ")");
-        Console.WriteLine("(" + p2.x + ", " + p2.y + ")");
+        Console.WriteLine("(" + p1.X + ", " + p1.Y + ")");
+        Console.WriteLine("(" + p2.X + ", " + p2.Y + ")");
     }
 
     private static void SwapPoints(Point p1, Point p2)
     {
-        var p1NewX = p2.Getx();
-        var p1NewY = p2.Gety();
-        var p2NewX = p1.Getx();
-        var p2NewY = p1.Gety();
-        p1.Setx(p1NewX);
-        p1.Sety(p1NewY);
-        p2.Setx(p2NewX);
-        p2.Sety(p2NewY);
+        var p1NewX = p2.X;
+        var p1NewY = p2.Y;
+        var p2NewX = p1.X;
+        var p2NewY = p1.Y;
+        p1.X = p1NewX;
+        p1.Y = p1NewY;
+        p2.X = p2NewX;
+        p2.Y = p2NewY;
     }
 }
