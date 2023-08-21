ab0b23da5f0999014b93a57798a705a706397780
kachers <renarskacerovskis@gmail.com>
2023-08-21 14:52:14 +0200

    Exercise3 completed

---
+++ csharp-basics/exercises/Polymorphism/Exercise3/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    internal class Employee : Person
+    {
+        public string JobTitle { get; }
+
+        public Employee(string firstName, string lastName, string address, int id, string jobTitle) : base(firstName, lastName, address, id)
+        {
+            JobTitle = jobTitle;
+        }
+
+        public override void Display(Person employee1)
+        {
+            if (employee1 is Employee employee)
+            {
+                Console.WriteLine($"First name: {employee.FirstName}\nLast name: {employee.LastName}\nAddress: {employee.Address}\nID: {employee.Id}\nJobTitle: {employee.JobTitle}");
+            }
+            else
+            {
+                Console.WriteLine("Person is not an employee");
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Exercise3/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    public class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Address { get; }
+        public int Id { get; }
+
+        public Person()
+        {
+            FirstName = "";
+            LastName = "";
+            Address = "";
+            Id = 0;
+        }
+        public Person(string firstName, string lastName, string address, int id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Id = id;
+        }
+
+        public virtual void Display(Person person1)
+        {
+
+            Console.WriteLine($"First name: {person1.FirstName}\nLast name: {person1.LastName}\nAddress: {person1.Address}\nID: {person1.Id}");
+        }
+    }
+}
