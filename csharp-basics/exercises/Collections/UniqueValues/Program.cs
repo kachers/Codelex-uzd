51c73a764a49da8628ff881479b17bb5f18d7a47
kachers <renarskacerovskis@gmail.com>
2023-08-29 15:05:29 +0200

    UniqueValues correction

--- csharp-basics/exercises/Collections/UniqueValues/Program.cs
+++ csharp-basics/exercises/Collections/UniqueValues/Program.cs
@@ -9,11 +9,12 @@ namespace UniqueValues
         private static void Main(string[] args)
         {
             var values = new List<string> { "Hi", "Meow", "Hello", "Meow", "Hi!", "Meow", "Hi", "Bye" };
-            var result = (from value in values
-                let count = values.Count(str => str == value)
-                where count == 1
-                select value).ToList();
-            Console.WriteLine(string.Join(",", result));
+           
+            var result = values.GroupBy(value => value).Where(group => group.Count() == 1).ToList();
+            foreach (var group in result)
+            {
+                Console.WriteLine(string.Join(",", group.Key));
+            }
         }
     }
 }
