e77c337e524478332e2dbdc51d416be1a68a5de6
kachers <renarskacerovskis@gmail.com>
2023-08-31 19:26:19 +0200

    DaysNames empty lines removed.

--- csharp-basics/exercises/Collections/DaysNames/Program.cs
+++ csharp-basics/exercises/Collections/DaysNames/Program.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
+﻿using System.Globalization;
 
-namespace DaysNames
-{ 
-    class Program
-    { 
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Enter year ");
-            int year = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter month ");
-            int month = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter day");
-            int day = int.Parse(Console.ReadLine());
+namespace DaysNames;
 
-            DateTime dt = new DateTime(year, month, day);
-            var LV = new System.Globalization.CultureInfo("lv-LV");
-            string diena = LV.DateTimeFormat.GetDayName(dt.DayOfWeek);
-            Console.WriteLine($"The day of the {day}/{month}/{year} was: {diena}");
-        }
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter year ");
+        var year = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter month ");
+        var month = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter day");
+        var day = int.Parse(Console.ReadLine());
+
+        var dt = new DateTime(year, month, day);
+        var LV = new CultureInfo("lv-LV");
+        var diena = LV.DateTimeFormat.GetDayName(dt.DayOfWeek);
+        Console.WriteLine($"The day of the {day}/{month}/{year} was: {diena}");
     }
 }
